e5e2c030d80ccc8bc8415f78b99bbfb985fe53e4
tier5 <work@tier5.us>
2017-03-30 19:51:29 +0530

    released innerpage v10.0

--- js/custom.js
+++ js/custom.js
@@ -250,3 +250,22 @@ $(document).ready(function(){
 });
 
 /* Contact form Ends */
+
+
+/* Testimonial slider */
+
+$(document).ready(function() {
+    $('.cust-slider .carousel-inner .item .slide1').click(function(e){
+        $(this).parent().parent().find($('.slide1')).removeClass("clicked");
+        $(this).addClass("clicked");
+        var clientReview = $(this).find($('.client-review')).html();
+
+        var review_display = $(this).parent().parent().find(".thumbnail-details");
+        review_display.html(clientReview);
+
+        e.preventDefault();
+    });
+
+});
+
+
